b71face881f3a2b4b3a6d201c35eeaf3814c2f60
pmurph829 <murphy.p@northeastern.edu>
2021-01-26 18:09:15 -0500

    Lab 1 main.c and output.c

--- Lab1_UnixTerminal/main.c
+++ Lab1_UnixTerminal/main.c
@@ -1,1 +1,36 @@
-// Add your program here!
+/* Peter Murphy
+ * CS5008
+ * Lab 1
+ */
+#include <stdio.h>
+
+/* Function that calculates a base raised to a number.
+ */
+
+double power(double base, double n){
+
+	double result = base;
+	
+	// If n is 0, the result will always be 1.
+	if(n == 0){ 
+		return 1;
+	}
+	
+	n--; // Correct for off by one error.
+	for(;n>0; n--){
+		result = result * base;
+	}
+	return result;
+}
+
+/* Calculate 2^n for 1 <= n <= 10.
+ */
+
+int main(){
+	for(int n = 0; n <= 10; n++){
+		double twoPower = power(2, n);
+		printf("2^%d = %.2f\n", n, twoPower);
+	}
+	
+	return 0;
+}
