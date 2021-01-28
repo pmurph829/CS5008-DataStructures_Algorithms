c631cce8b594a8e7ae06fad5b46f3566b8981151
pmurph829 <murphy.p@northeastern.edu>
2021-01-27 21:48:03 -0500

    Improvements to Assignment 1

--- Assignment1_GuessingGame/part1/main.c
+++ Assignment1_GuessingGame/part1/main.c
@@ -9,20 +9,42 @@
 
 int main(){
 	int guess;
-	// READ INSTRUCTIONS ON WHAT TO DO WITH THIS ARRAY.
+	int numGames;
 	int guesses[5];
+	int keepGuessing = 1;
+	int i = 0;
 
 	// Generate a random number n with: 1<=n<= 10
-	srand(time(NULL));
-	int answer = 1 + rand() % 10;
-	// REMOVE THIS PRINT IN FINAL	
-	printf("Answer: %d\n", answer);
-	
-	for(int i = 0; i <= 5; i++){
-		printf("Make guess (#%d): ", i);
-		scanf("%d", &guess);
-		guesses[i] = guess;
+	srand(time(NULL));	
+
+	for(numGames = 0; numGames < 5; numGames++){
+		printf("================================\n");
+		printf("New Game! Pick a number (1 - 10)\n");
+		printf("================================\n");
+		int answer = 1 + rand() % 10;
+		// REMOVE THIS!!!!!!!!!!!!!
+		printf("Answer: %d\n", answer);
+		
+		while(keepGuessing == 1){
+			i++;
+			printf("Guess #%d: ", i);
+			scanf("%d", &guess);
+			if(guess == answer){
+				printf("You got it!!\n");
+				guesses[numGames] = i;
+				keepGuessing = 0;
+			} else if(guess < answer){
+				printf("Nope, too low!\n");
+			}
+			else {
+				printf("Nope, too high!\n");
+			}
+
+		}
+		keepGuessing = 1;
+			
 	}
-	
+		
 	return 0;
 }
+
