72d9cc41e5e10d5123104abf888f223734ed1b2b
murphyp <murphy.p@northeastern.edu>
2021-02-07 15:06:21 -0500

    Starting lab 3

--- Lab3_Assembly/part1/tool.c
+++ Lab3_Assembly/part1/tool.c
@@ -1,1 +1,25 @@
-// Implement your cycle count tool here.
+#include <stdio.h>
+
+
+int main(int argc, char** argv){
+	/*
+	int i;
+	for(i=0; i<argc; i++){
+		printf("%s\n", argv[i]);
+	}
+	*/
+	
+	FILE* fptr = fopen(argv[1], "r");
+	
+	while(!feof(fptr)){
+		char line[1000];
+
+		fgets(line, sizeof(line), fptr);
+		printf("%s\n", line);
+
+	}
+	
+	fclose(fptr);
+	
+}
+
