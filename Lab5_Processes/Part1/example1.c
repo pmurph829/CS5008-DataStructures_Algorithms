d1134bd856d4729862e0f6c3fb13a5b3405c5e9d
murphyp <murphy.p@northeastern.edu>
2021-02-20 17:40:32 -0500

    Started lab 5

--- Lab5_Processes/Part1/example1.c
+++ Lab5_Processes/Part1/example1.c
@@ -1,1 +1,28 @@
 // Type in and try example1.c
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+    
+    // Variable to store process ID
+    pid_t pid;
+
+    // Variable x allocated on the stack
+    int x = 1;
+
+    // pid returns parent process ID:
+    //  0 if the process is the child
+    //  >0 is the process ID of the child (returned in parent)
+    pid = fork();
+
+    // Child process executing
+    if (pid==0){
+        printf("child: x = %d\n", ++x);
+        return 0;
+    }
+
+    // Parent process executing
+    printf("Parent: x = %d\n", ++x);
+}
--- Lab5_Processes/Part1/example4.c
+++ Lab5_Processes/Part1/example4.c
@@ -1,1 +1,22 @@
 // Type in and try example4.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(){
+    
+    int child_status;
+
+    // Store the pid with fork
+    if (fork()==0){
+        printf("Child runs!\n");
+        exit(0);
+    } else {
+        printf("Hello from parent\n");
+        wait(&child_status);
+    }
+}
