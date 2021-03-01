9397e483678700075449262c2cd703d449ce5476
pmurph829 <murphy.p@northeastern.edu>
2021-03-01 17:42:01 -0500

    Started lab 6 - finished task 3

--- Lab6_MultiprocessingArtists/vfork.c
+++ Lab6_MultiprocessingArtists/vfork.c
@@ -1,2 +1,30 @@
 // Implement your part 1 solution here
 // gcc vfork.c -o vfork
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void paint(int pid){
+    printf("Artist %d is painting!\n", pid);
+}
+
+int main(int argc, char** arcv){
+    int numberOfArtists = 8;
+    int* integers = malloc(sizeof(int)*8000);
+    pid_t pid;
+    int i = 0;
+    for (; i<numberOfArtists; i++){
+        pid = fork();
+        if (pid == 0){
+            paint(getpid());
+            exit(0);
+        } else {
+            wait(NULL);
+            printf("Parent is exiting\n");
+        }
+    }
+    free(integers);
+    return 0;
+}
