cf0dbb710dea034ca2cd890bb6232991987511d2
pmurph829 <murphy.p@northeastern.edu>
2021-04-07 14:37:34 -0400

    Finished pt 1 lab10

--- Lab10_RecursiveStrategy/factorial.c
+++ Lab10_RecursiveStrategy/factorial.c
@@ -4,22 +4,29 @@
 #include <stdio.h>
 
 // TODO: Implement iterative solution here
-int factorial(int n){
-
-  return 0;
+long long int factorial(int n){
+    long long int acc = 1;
+    int i = 0;
+    for (i = n; i > 0; i--) {
+        acc = acc * i;
+    }
+    return acc;
 };
 
 // TODO: Implement recursive solution here
-int factorial_rec(int n){
-
-  return 0;
+long long int factorial_rec(int n){
+    if (n==0 || n==1) {
+        return 1;
+    }
+    return n * factorial_rec(n - 1);
 }
 
 int main(){
 
   // Both of these should print the same result!
-  printf("factorial(10) = %d\n",factorial(10));
-  printf("factorial_rec(10) = %d\n",factorial_rec(10));
+  int calc = 21;
+  printf("factorial(%d)     = %lld\n", calc, factorial(calc));
+  printf("factorial_rec(%d) = %lld\n", calc, factorial_rec(calc));
 
 
   return 0;
